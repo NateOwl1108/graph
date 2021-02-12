b96b23b8150eebb65af7ce32058d21ef820e921b
NateOwl <natedog112004@gmail.com>
2021-02-12 03:12:25 +0000

    Created a build from edges that makes a list where every node's index is its value. I then modified the get nodes breadth first so that is appends a value in list to visited, removes it from queue. I then checks it's neighbors to see if they are in queue or visited. If not then it appends it to queue

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,5 @@
 
+
 def get_neighbors(value, edges_list):
   copy_list = list(edges_list)
   neighbor_list = []
@@ -8,28 +9,36 @@ def get_neighbors(value, edges_list):
         if point!= value:
           neighbor_list.append(point)
   return neighbor_list
+
 class Node():
   def __init__(self, value):
     self.value = value
     self.neighbors = None
     self.index = None
+
 class Graph():
   def __init__(self, edges):
     self.edges = edges
+    self.list = [_ for _ in range(len(self.edges))]
+
+  def build_from_edges(self):
+    for index in range(len(self.edges)):
+      index_value = Node(index)
+      index_value.neighbors = get_neighbors(index_value.value, self.edges)
+      self.list[index] = index_value
+
+
   def get_nodes_breadth_first(self, root):
-      node_array = [root]
-      neighbors_array = []
-      visited = []
-      non_repeating = True
-      while non_repeating == True:
-        non_repeating = False
-        for point in range(len(node_array)):
-          if point not in [node.value for node in visited] :
-            non_repeating = True
-            node_array[index] = Node(node_array[index])
-            node_array[index].neighbors = get_neighbors(node_array[index].value, self.edges)
-            
-            
-        node_array = neighbors_array
+    queue = [root]
+    visited = []
+    while len(queue)>0:
+      value = self.list[queue[0]]
+      visited.append(value)
+      queue.pop(0)
+      for neighbor in value.neighbors:
+        if neighbor not in [node.value for node in visited] and neighbor not in queue:
+          queue.append(neighbor)
+    return visited
+
       
     
--- test_graph.py
+++ test_graph.py
@@ -3,9 +3,9 @@ sys.path.append('src')
 from graph import Graph
 edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
 graph = Graph(edges)
-
-
+graph.build_from_edges()
+print(graph.list[0].neighbors)
 
 bf = graph.get_nodes_breadth_first(2)
-print([node.index for node in bf])
+print([node.value for node in bf])
 [2, 1, 0, 3, 4, 5]
