33b7ff8ebdd16a36dcba5bf8abb9c0d505146789
NateOwl <natedog112004@gmail.com>
2021-02-13 22:11:19 +0000

    I added get neighbors to the graph class and added the function get depth first which changes appending to the end of que to the front.

--- src/graph.py
+++ src/graph.py
@@ -1,35 +1,68 @@
 
-def get_neighbors(value, edges_list):
-  copy_list = list(edges_list)
-  neighbor_list = []
-  for neighbors in copy_list:
-    if value in neighbors:
-      for point in neighbors:
-        if point!= value:
-          neighbor_list.append(point)
-  return neighbor_list
 class Node():
   def __init__(self, value):
-    self.value = value
+    self.index = value
     self.neighbors = None
-    self.index = None
 class Graph():
   def __init__(self, edges):
     self.edges = edges
+    self.node_list = [_ for _ in range(len(self.edges))]
+
+  def get_neighbors(self, value):
+    copy_list = list(self.edges)
+    neighbor_list = []
+    for neighbors in copy_list:
+      if value in neighbors:
+        for point in neighbors:
+          if point!= value:
+            neighbor_list.append(point)
+    return neighbor_list
+
+  def build_from_edges(self):
+    for index in range(len(self.edges)):
+      self.node_list[index] = Node(index)
+      self.node_list[index].index = index
+      self.node_list[index].neighbors = self.get_neighbors(index)
+    print([node.neighbors for node in self.node_list])
+
   def get_nodes_breadth_first(self, root):
-      node_array = [root]
-      neighbors_array = []
-      visited = []
-      non_repeating = True
-      while non_repeating == True:
-        non_repeating = False
-        for point in range(len(node_array)):
-          if point not in [node.value for node in visited] :
-            non_repeating = True
-            node_array[index] = Node(node_array[index])
-            node_array[index].neighbors = get_neighbors(node_array[index].value, self.edges)
-            
-            
-        node_array = neighbors_array
+    queue = [root]
+    visited = []
+    runs = 0 
+    while len(queue) > 0 :
+      runs += 1
+      if runs == 30:
+        return 0
+      else:
+        node = self.node_list[queue[0]]
+        visited.append(node)
+        neighbors = self.node_list[node.index].neighbors
+        queue.pop(0)
+        for neighbor in neighbors:
+         if neighbor not in [node.index for node in visited]:
+            queue.append(neighbor)
+        
+      return visited
+      
+  def get_nodes_depth_first(self, root):
+    queue = [root]
+    visited = []
+    runs = 0 
+    while len(queue) > 0 :
+      runs += 1
+      if runs == 30:
+        return 0
+      else:
+        node = self.node_list[queue[0]]
+        visited.append(node)
+        neighbors = self.node_list[node.index].neighbors
+        queue.pop(0)
+        for neighbor in neighbors:
+         if neighbor not in [node.index for node in visited]:
+            queue.insert(0,neighbor)
+        
+      return visited
+      
+    
       
     
--- test_graph.py
+++ test_graph.py
@@ -4,8 +4,10 @@ from graph import Graph
 edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
 graph = Graph(edges)
 
-
+graph.build_from_edges()
+print([node.index for node in graph.node_list])
 
 bf = graph.get_nodes_breadth_first(2)
+
 print([node.index for node in bf])
-[2, 1, 0, 3, 4, 5]
+#[2, 1, 0, 3, 4, 5]
