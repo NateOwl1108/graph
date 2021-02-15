b0d84627c6ed2405c5a02f945433c6b2b4329a83
NateOwl <natedog112004@gmail.com>
2021-02-15 23:47:50 +0000

    adding the function calc distance which calculates the distance between two points the function set_breadth_first_distance_and_previous which assigns each value a distance from start and its previous value.

--- src/graph.py
+++ src/graph.py
@@ -1,54 +1,87 @@
 
 class Node():
-  def __init__(self, value):
+  def __init__(self, current_node):
     self.neighbors = None
-    self.index = value
+    self.index = current_node
+    self.distance = None
+    self.previous = None
 
 class Graph():
   def __init__(self, edges):
     self.edges = edges
     self.node_list = [_ for _ in range(len(self.edges))]
 
-  def get_neighbors(self, value):
+  def get_neighbors(self, current_node):
     copy_list = list(self.edges)
     neighbor_list = []
     for neighbors in copy_list:
-      if value in neighbors:
+      if current_node in neighbors:
         for point in neighbors:
-          if point!= value:
+          if point!= current_node:
             neighbor_list.append(point)
     return neighbor_list
 
   def build_from_edges(self):
     for index in range(len(self.edges)):
-      index_value = Node(index)
-      index_value.neighbors = self.get_neighbors(index_value.index)
-      self.node_list[index] = index_value
+      index_current_node = Node(index)
+      index_current_node.neighbors = self.get_neighbors(index_current_node.index)
+      self.node_list[index] = index_current_node
       
   def get_nodes_breadth_first(self, root):
     queue = [root]
     visited = []
     while len(queue)>0:
-      value = self.node_list[queue[0]]
-      visited.append(value)
+      current_node = self.node_list[queue[0]]
+      visited.append(current_node)
       queue.pop(0)
-      for neighbor in value.neighbors:
+      for neighbor in current_node.neighbors:
         if neighbor not in [node.index for node in visited] and neighbor not in queue:
           queue.append(neighbor)
     return visited
   
-
   def get_nodes_depth_first(self, root):
     queue = [root]
     visited = []
     while len(queue)>0:
-      value = self.node_list[queue[0]]
-      visited.append(value)
+      current_node = self.node_list[queue[0]]
+      visited.append(current_node)
       queue.pop(0)
-      for neighbor in value.neighbors:
+      for neighbor in current_node.neighbors:
         if neighbor not in [node.index for node in visited] and neighbor not in queue:
           queue.insert(0,neighbor)
     return visited
-
-      
+  
+  def set_breadth_first_distance_and_previous(self, starting_node_index):
+    first_node = Node(starting_node_index)
+    first_node.distance = 0
+    queue = [first_node]
+    visited = []
+    distance = 0
+    while len(queue)>0:
+      current_node = self.node_list[queue[0].index]
+      current_node.distance = queue[0].distance
+      current_node.previous = queue[0].previous
+      visited.append(current_node)
+      queue.pop(0)
+      for neighbor in current_node.neighbors:
+        if neighbor not in [node.index for node in visited] and neighbor not in queue and neighbor not in [node.index for node in queue]:
+          neighbor = Node(neighbor)
+          neighbor.distance = current_node.distance +1
+          neighbor.previous = current_node
+          queue.append(neighbor)
+    return visited
     
+  def calc_distance(self, starting_node_index, ending_node_index):
+      node_list = self.set_breadth_first_distance_and_previous(starting_node_index)
+      for node_index in range(len(node_list)):
+        if node_list[node_index].index == ending_node_index:
+          return node_list[node_index].distance
+
+
+  def calc_shortest_path(self, starting_node_index, ending_node_index):
+    node_list = self.set_breadth_first_distance_and_previous(starting_node_index)
+    previous_list = [node_list[ending_node_index].index]
+    print(previous_list)
+    while previous_list[0] != starting_node_index:
+      previous_list.insert(0, node_list[previous_list[0]].previous.index)
+    return previous_list
--- test_graph.py
+++ test_graph.py
@@ -5,12 +5,10 @@ edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
 graph = Graph(edges)
 
 graph.build_from_edges()
-print([node.index for node in graph.node_list])
 
 bf = graph.get_nodes_breadth_first(2)
 
-print([node.index for node in bf])
-#[2, 1, 0, 3, 4, 5]
+assert [node.index for node in bf] == [2, 1, 0, 3, 4, 5]
 graph.build_from_edges()
 
 bf = graph.get_nodes_breadth_first(2)
@@ -19,3 +17,18 @@ assert [node.index for node in bf] == [2, 1, 0, 3, 4, 5]
 df = graph.get_nodes_depth_first(2)
 assert [node.index for node in df] == [2, 1, 4, 5, 3, 0]
 
+assert graph.calc_distance(0,4) == 2
+assert graph.calc_distance(5,2) == 3
+assert graph.calc_distance(0,5) == 3
+assert graph.calc_distance(4,1) == 1
+assert graph.calc_distance(3,3) == 0
+
+#print(graph.calc_shortest_path(0,4))
+#assert graph.calc_shortest_path(0,4) == [0, 1, 4]
+#print(graph.calc_shortest_path(5,2))
+#assert graph.calc_shortest_path(5,2) == [5, 4, 1, 2]
+#assert graph.calc_shortest_path(0,5) == [0, 1, 4, 5]
+#print(graph.calc_shortest_path(4,1))
+#assert graph.calc_shortest_path(4,1) == [4, 1]
+#print(graph.calc_shortest_path(3,3))
+assert graph.calc_shortest_path(3,3) == [3]
